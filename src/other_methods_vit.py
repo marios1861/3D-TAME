34efb5ab12e932a4ba70212e9795ca30fa74fe52
marios1861 <44480600+marios1861@users.noreply.github.com>
2023-04-05 11:54:34 +0300

    Add gelu versions, change evaluation of other methods to include minmax norm

--- src/other_methods_vit.py
+++ src/other_methods_vit.py
@@ -7,7 +7,6 @@ Usage:
 from pathlib import Path
 from typing import Any, Dict, List, Optional, Tuple, Type
 import traceback
-import numpy
 
 import pandas as pd
 import torch
@@ -128,8 +127,10 @@ def run(
     if example_gen is not None:
         image, _ = dataloader.dataset[example_gen]
         image = image.unsqueeze(0)
-        mask = torch.tensor(cam_model(input_tensor=image))
+        mask = torch.tensor(cam_model(input_tensor=image)).unsqueeze(dim=1)
+        masks = metrics.normalizeMinMax(mask)
         image = image.squeeze()
+        mask = mask.squeeze()
         save_heatmap(
             mask,
             image,
@@ -149,16 +150,12 @@ def run(
         logits = logits.softmax(dim=1)
         chosen_logits, model_truth = logits.max(dim=1)
         masks = cam_model(input_tensor=images)  # type: ignore
-        if numpy.isnan(masks).any():
+        masks = metrics.normalizeMinMax(torch.from_numpy(masks).cuda().unsqueeze(dim=1))
+        if torch.isnan(masks).any():
             print("NaNs in masks")
             quit()
-        # disable road temporarily
-        metric_ROAD(images, model_truth, masks)
-        if use_cuda:
-            masks = torch.tensor(masks).cuda().unsqueeze(dim=1)
-        else:
-            masks = torch.tensor(masks).unsqueeze(dim=1)
         metric_AD_IC(images, chosen_logits, model_truth, masks)
+        metric_ROAD(images, model_truth, masks.squeeze().cpu().detach().numpy())
 
     ADs, ICs = metric_AD_IC.get_results()
     ROADs = metric_ROAD.get_results()
--- src/utilities/composite_models.py
+++ src/utilities/composite_models.py
@@ -19,6 +19,74 @@ class AttentionMech(nn.Module):
         raise NotImplementedError
 
 
+class TAttentionV2_1(AttentionMech):
+    def __init__(self, ft_size: List[torch.Size]):
+        super().__init__(ft_size)
+        # noinspection PyTypeChecker
+        self.mhas = nn.ModuleList(
+            [nn.MultiheadAttention(ft[2], 12, batch_first=True) for ft in ft_size]
+        )
+        ln_dim = ft_size[0][2]
+        self.lns = nn.ModuleList(
+            [
+                nn.LayerNorm(
+                    ln_dim,
+                    eps=1e-06,
+                )
+                for _ in ft_size
+            ]
+        )
+        self.gelu = nn.GELU()
+
+        def reshape_transform(tensor, height=14, width=14):
+            result = tensor[:, 1:, :].reshape(
+                tensor.size(0), height, width, tensor.size(2)
+            )
+
+            # Bring the channels to the first dimension,
+            # like in CNNs.
+            result = result.transpose(2, 3).transpose(1, 2)
+            return result
+
+        self.reshape = reshape_transform
+        fuse_channels = sum(ft[2] for ft in ft_size)
+
+        self.cnn_fuser = nn.Conv2d(
+            in_channels=fuse_channels,
+            out_channels=1000,
+            kernel_size=1,
+            padding=0,
+            bias=True,
+        )
+
+    def forward(self, features):
+        feature_maps = features
+        # Multihead Attention
+        class_maps = [
+            op(feature, need_weights=False)
+            for op, feature in zip(self.mhas, feature_maps)
+        ]
+        # layer norm
+        class_maps = [op(feature) for op, feature in zip(self.lns, class_maps)]
+        # add (skip connection)
+        class_maps = [
+            class_map + feature_map
+            for class_map, feature_map in zip(class_maps, feature_maps)
+        ]
+        # activation
+        class_maps = [self.gelu(class_map) for class_map in class_maps]
+        # Reshape
+        class_maps = [self.reshape(class_map) for class_map in class_maps]
+        # Concat
+        class_map = torch.cat(class_maps, 2)
+        # fuse into 1000 channels
+        c = self.cnn_fuser(class_map)  # batch_size x1xWxH
+        # activation
+        a = torch.sigmoid(c)
+
+        return a, c
+
+
 class TAttentionV2(AttentionMech):
     def __init__(self, ft_size: List[torch.Size]):
         super().__init__(ft_size)
@@ -79,6 +147,81 @@ class TAttentionV2(AttentionMech):
         class_maps = [self.reshape(class_map) for class_map in class_maps]
         # Concat
         class_map = torch.cat(class_maps, 2)
+        # fuse into 1000 channels
+        c = self.cnn_fuser(class_map)  # batch_size x1xWxH
+        # activation
+        a = torch.sigmoid(c)
+
+        return a, c
+
+
+class TAttentionV1_1(AttentionMech):
+    def __init__(self, ft_size: List[torch.Size]):
+        super().__init__(ft_size)
+        # noinspection PyTypeChecker
+        self.mhas = nn.ModuleList(
+            [nn.MultiheadAttention(ft[2], 12, batch_first=True) for ft in ft_size]
+        )
+        ln_dim = ft_size[0][2]
+        self.lns = nn.ModuleList(
+            [
+                nn.LayerNorm(
+                    ln_dim,
+                    eps=1e-06,
+                )
+                for _ in ft_size
+            ]
+        )
+        self.gelu = nn.GELU()
+
+        def reshape_transform(tensor, height=14, width=14):
+            result = tensor[:, 1:, :].reshape(
+                tensor.size(0), height, width, tensor.size(2)
+            )
+
+            # Bring the channels to the first dimension,
+            # like in CNNs.
+            result = result.transpose(2, 3).transpose(1, 2)
+            return result
+
+        self.reshape = reshape_transform
+        fuse_channels = sum(ft[2] for ft in ft_size)
+        # noinspection PyTypeChecker
+
+        self.mha_fuser = nn.MultiheadAttention(fuse_channels, 12, batch_first=True)
+
+        self.cnn_fuser = nn.Conv2d(
+            in_channels=fuse_channels,
+            out_channels=1000,
+            kernel_size=1,
+            padding=0,
+            bias=True,
+        )
+
+    def forward(self, features):
+        feature_maps = features
+        # Multihead Attention
+        class_maps = [
+            op(feature, feature, feature, need_weights=False)[0]
+            for op, feature in zip(self.mhas, feature_maps)
+        ]
+        # layer norm
+        class_maps = [op(feature) for op, feature in zip(self.lns, class_maps)]
+        # add (skip connection)
+        class_maps = [
+            class_map + feature_map
+            for class_map, feature_map in zip(class_maps, feature_maps)
+        ]
+        # activation
+        class_maps = [self.gelu(class_map) for class_map in class_maps]
+        # concat
+        class_map = torch.cat(class_maps, 2)
+        # Multihead Attention
+        class_map = self.mha_fuser(class_map, class_map, class_map, need_weights=False)[
+            0
+        ]
+        # Reshape
+        class_map = self.reshape(class_map)
         # fuse into 1000 channels
         c = self.cnn_fuser(class_map)  # batch_size x1xWxH
         # activation
@@ -401,7 +544,9 @@ class AttentionMechFactory(object):
         "NoskipNobatchnorm": AttentionV3d2,
         "Sigmoidinfeaturebranch": AttentionV5d1,
         "TAttentionV1": TAttentionV1,
+        "TAttentionV1_1": TAttentionV1_1,
         "TAttentionV2": TAttentionV2,
+        "TAttentionV2_1": TAttentionV2_1,
     }
 
     @classmethod
