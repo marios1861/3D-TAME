00bac7900b9c77670106e22656f74cb7318f11cb
marios1861 <44480600+marios1861@users.noreply.github.com>
2023-09-28 20:12:38 +0300

    Add new scripts

---
+++ pl_scripts/lcam_vit_b_16.py
@@ -0,0 +1,89 @@
+import os
+from pathlib import Path
+
+from torch import nn
+
+import lightning.pytorch as pl
+import torch
+from dotenv import load_dotenv
+from lightning.pytorch.callbacks import ModelCheckpoint
+from lightning.pytorch.loggers import CSVLogger, TensorBoardLogger
+
+from tame.utilities import send_email
+from tame.utilities.attention.factory import AMBuilder
+from tame.utilities.pl_module import TAMELIT, LightnightDataset
+from tame.utilities.attention.generic_atten import AttentionMech
+
+load_dotenv()
+
+os.environ["MASTER_ADDR"] = "160.40.53.85"
+os.environ["MASTER_PORT"] = "12345"
+os.environ["WORLD_SIZE"] = "3"
+torch.set_float32_matmul_precision("medium")
+version = "lcam"
+model_name = "vit_b_16"
+layers = [
+    "encoder.layers.encoder_layer_11",
+]
+epochs = 8
+
+
+class LCAM(AttentionMech):
+    def __init__(self, ft_size):
+        super(AttentionMech, self).__init__()
+        in_channel = ft_size[0][1]
+        self.op = nn.Conv2d(
+            in_channels=in_channel,
+            out_channels=1000,
+            kernel_size=1,
+            padding=0,
+            bias=True,
+        )
+
+    def forward(self, features):
+        features = list(features)
+        c = self.op(features[0])
+        a = torch.sigmoid(c)
+        return a, c
+
+
+AMBuilder.register_attention(version, LCAM)
+model = TAMELIT(
+    model_name=model_name,
+    layers=layers,
+    attention_version=version,
+    train_method="legacy",
+    schedule="NEW",
+    lr=0.001,
+    epochs=epochs,
+)
+# model: pl.LightningModule = torch.compile(model)  # type: ignore
+
+dataset = LightnightDataset(
+    dataset_path=Path(os.getenv("DATA", "./")),
+    datalist_path=Path(os.getenv("LIST", "./")),
+    model=model_name,
+    batch_size=32,
+    legacy=True,
+)
+
+checkpointer = ModelCheckpoint(every_n_epochs=1, save_on_train_epoch_end=False)
+
+# torch._dynamo.config.verbose=True
+trainer = pl.Trainer(
+    # precision="16-mixed",
+    # gradient_clip_algorithm="norm",
+    max_epochs=epochs,
+    callbacks=[checkpointer],
+)
+trainer.logger = TensorBoardLogger("logs", name=(version + "_" + model_name), sub_dir="tb_logs")
+trainer.fit(model, dataset)
+
+
+trainer.logger = CSVLogger("logs", name=(version + "_" + model_name))
+trainer.test(model, dataset)
+send_email(
+    version + "_" + model_name,
+    os.environ["PASS"],
+    "resnet lcam run complete, check results and run resnet V5 next",
+)
---
+++ pl_scripts/ran/vit_scorecam.py
@@ -0,0 +1,48 @@
+import os
+from pathlib import Path
+
+import lightning.pytorch as pl
+import torch
+from dotenv import load_dotenv
+from lightning.pytorch.callbacks import ModelCheckpoint
+from lightning.pytorch.loggers import CSVLogger, TensorBoardLogger
+
+from tame.utilities import send_email, COMPAREVIT
+from tame.utilities.pl_module import TAMELIT, LightnightDataset
+
+
+load_dotenv()
+
+os.environ["MASTER_ADDR"] = "160.40.53.85"
+os.environ["MASTER_PORT"] = "12345"
+os.environ["WORLD_SIZE"] = "3"
+torch.set_float32_matmul_precision("medium")
+
+version = "scorecam"
+model_name = "vit_b_16"
+layers = [
+    "encoder.layers.encoder_layer_9",
+    "encoder.layers.encoder_layer_10",
+    "encoder.layers.encoder_layer_11",
+]
+epochs = 8
+
+model = COMPAREVIT(name=version)
+# model: pl.LightningModule = torch.compile(model)  # type: ignore
+
+dataset = LightnightDataset(
+    dataset_path=Path(os.getenv("DATA", "./")),
+    datalist_path=Path(os.getenv("LIST", "./")),
+    model=model_name,
+    batch_size=32,
+)
+
+# torch._dynamo.config.verbose=True
+trainer = pl.Trainer()
+trainer.logger = CSVLogger("logs", name=(version + "_" + model_name))
+trainer.test(model, dataset)
+send_email(
+    version + "_" + model_name,
+    os.environ["PASS"],
+    "vit_b_16 gradcam run complete, check results and run vgg16 gradcam++ next",
+)
